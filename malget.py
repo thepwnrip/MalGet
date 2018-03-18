fb435d6e233324a0110dcea6019b3ccaa4b40801
Himanshu Khokhar <himanshu.khokhar@mail.com>
2018-03-18 21:53:31 +0530

    Update malget.py

--- malget.py
+++ malget.py
@@ -14,89 +14,89 @@ MMM  MMM   aa aa lll  GG  GG   eee  tt
 MM MM MM  aa aaa lll GG      ee   e tttt  
 MM    MM aa  aaa lll GG   GG eeeee  tt    
 MM    MM  aaa aa lll  GGGGGG  eeeee  tttt
-				
-				-- By Himanshu Khokhar (@rinne_parad0x)
+
+                                -- By Himanshu Khokhar (@rinne_parad0x)
 """
 
-url_api 	= "https://dasmalwerk.eu/api"				# To list latest malwares from Das Malwerk
+url_api         = "https://dasmalwerk.eu/api"                           # To list latest malwares from Das Malwerk
 url_malware = "http://dasmalwerk.eu/zippedMalware/"
 
 
 def saveMalware(malName):
-	print("Saving file : " + malName + " ...")
+        print("Saving file : " + malName + " ...")
 
-	malware = requests.get(url_malware + malName)
-	
-	with open(malName + ".zip" , "wb") as mal:
-		mal.write(malware.content)
+        malware = requests.get(url_malware + malName)
 
-	print("Malware has been saved as : " + malName + "\n")
+        with open(malName, "wb") as mal:
+                mal.write(malware.content)
+
+        print("Malware has been saved as : " + malName + "\n")
 
 
 
 def obtainMalware():
-	print(banner)
+        print(banner)
 
 
-	details = requests.get(url_api)
+        details = requests.get(url_api)
 
 
-	# Get JSON data from response
-	jsonData = details.text
+        # Get JSON data from response
+        jsonData = details.text
 
 
-	# Convert JSON data to Python Dictionary for processing
-	data = json.loads(jsonData)
+        # Convert JSON data to Python Dictionary for processing
+        data = json.loads(jsonData)
 
-	# Dictionary has one key - items - and it has all the details
-	data = data['items']
+        # Dictionary has one key - items - and it has all the details
+        data = data['items']
 
-	# Obtain the no. of malwares in response. Sub 1 because the last value is - {'debug': 'no'}
-	num = len(data) - 1 
+        # Obtain the no. of malwares in response. Sub 1 because the last value is - {'debug': 'no'}
+        num = len(data) - 1 
 
-	print("List of samples : ")
+        print("List of samples : ")
 
-	for i in range(0, num):
-		print("%02d.) %s" %(i + 1, data[i]['Hashvalue']))	
+        for i in range(0, num):
+                print("%02d.) %s" %(i + 1, data[i]['Hashvalue']))
 
-	choice = int(input("\nChoose the index value or 0 to download all the samples : "))
+        choice = int(input("\nChoose the index value or 0 to download all the samples : "))
 
-	choice  = choice - 1
+        choice  = choice - 1
 
-	#	All the malware samples will be saved under Samples dir in the directory of script
-	#
-	#	If you want to change, change the variable - sample
+        #       All the malware samples will be saved under Samples dir in the directory of script
+        #
+        #       If you want to change, change the variable - sample
 
-	sample = os.path.join(os.getcwd(), "Samples")
+        sample = os.path.join(os.getcwd(), "Samples")
 
-	if os.path.isdir(sample) is False:
-		os.mkdir(sample)
+        if os.path.isdir(sample) is False:
+                os.mkdir(sample)
 
-	os.chdir(sample)
+        os.chdir(sample)
 
-	if choice == -1:
+        if choice == -1:
 
-		print("Downloading all samples ... \n")
-		
-		for i in range(0, num):
-			malName = data[i]['Filename'] + ".zip"
-			saveMalware(malName)
-	
-	elif choice >= 0 and choice < len(data):
+                print("Downloading all samples ... \n")
 
-		malName = data[choice]['Filename'] + ".zip"		
-		print("Downloading " + malName + "\n")
+                for i in range(0, num):
+                        malName = data[i]['Filename'] + ".zip"
+                        saveMalware(malName)
 
-		saveMalware(malName)
+        elif choice >= 0 and choice < len(data):
 
-	else:
-		print("Choose a valid choice.")
-		os._exit(1)
+                malName = data[choice]['Filename'] + ".zip"
+                print("Downloading " + malName + "\n")
+
+                saveMalware(malName)
+
+        else:
+                print("Choose a valid choice.")
+                os._exit(1)
 
 
-	print("Note : The password of the zip file is - infected")
-	print("\nEnjoy malware analysis.")
+        print("Note : The password of the zip file is - infected")
+        print("\nEnjoy malware analysis.")
 
 
 if __name__ == '__main__':
-	obtainMalware()
+        obtainMalware()
